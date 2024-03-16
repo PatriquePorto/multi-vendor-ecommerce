26385432c59e21802ec73bbf60820d1fcb5c8614
Patrique Porto <patrikrock@hotmail.com>
2024-03-16 10:20:42 -0300

    Project structure

---
+++ app/(back-office)/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Layout({ children }) {
+  return (
+    <div className='flex'>
+         {/*Sidebar */}
+         <div className=''>Sidebar</div>
+         <div>
+            {/*Header */}
+            {/*Main */}
+         </div>
+         {/*Main Body */}
+    </div>
+  )
+}
